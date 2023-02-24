6311008db9f5cdbdab5841046dd16df45f91382b
imdanieldavy <imdanieldavy@gmail.com>
2023-02-24 16:44:00 +0200

    simple bulma design added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <section className="hero is-info">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana33"
+                image={CortanaImage}
+                description="Cortana was created by Microsoft. Who knows what it does?"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri66"
+                image={SiriImage}
+                description="Siri was created by Apple and is being phased out."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
